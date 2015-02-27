2523e6607968153254e8d5fdd969b3bbb8b69b4a
Nick Stares <Nick Stares>
2015-02-27 14:12:57 -0800

    models complete; added password in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227214321) do
+ActiveRecord::Schema.define(version: 20150227220612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 20150227214321) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password"
+    t.string   "password_digest"
   end
 
   create_table "users_itineraries_attractions", force: :cascade do |t|
