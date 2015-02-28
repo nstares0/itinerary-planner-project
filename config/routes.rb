aeccf7479709e4ccd381eab1bfc4bd09c20b6626
Sue Lucas <choi.soyoung@gmail.com>
2015-02-27 17:08:14 -0800

    modify routes to nested reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
   root 'itineraries#index'
 
+  resources :itineraries do
+    resources :attractions do
+      post '/reviews' => 'reviews#create', as: 'reviews'
+    end
+    post '/reviews' => 'reviews#create', as: 'reviews'
+  end
+
+
   # resources :itineraries do
-  #   resources :attractions do
-  #     post '/reviews' => 'reviews#create', as: 'reviews'
-  #   end
-  #   post '/reviews' => 'reviews#create', as: 'reviews'
+  #  resources :attractions
   # end
 
-
-  resources :itineraries do
-   resources :attractions
-  end
-
-  resources :itineraries do
-    post '/reviews' => 'reviews#create', as: 'reviews'
-  end 
+  # resources :itineraries do
+  #   post '/reviews' => 'reviews#create', as: 'reviews'
+  # end 
  
-  resources :attractions do
-    post '/reviews' => 'reviews#create', as: 'reviews'
-  end
+  # resources :attractions do
+  #   post '/reviews' => 'reviews#create', as: 'reviews'
+  # end
 
 
   # get 'attractions/show'
